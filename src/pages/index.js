d01cc6e99c883705546d77408be4e6e35398e236
Trần Nhật Duy <duytn.hcm@gmail.com>
2020-03-08 18:55:40 +0700

    Chore

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,32 @@ import { Alert } from '@material-ui/lab';
 
 import { getResultMessage } from '../utils';
 
-import styles from './styles/home';
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing(3)
+  },
+  paper: {
+    padding: theme.spacing(2)
+  },
+  form: {
+    padding: theme.spacing(2),
+    paddingBottom: 0
+  },
+  textField: {
+    fontSize: '2em'
+  },
+  footer: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: theme.spacing(2)
+  },
+  footerLink: {
+    paddingLeft: '4px',
+    textDecoration: 'none',
+    fontWeight: 700,
+    color: 'inherit'
+  }
+});
 
 class HomePage extends React.Component {
   state = {
